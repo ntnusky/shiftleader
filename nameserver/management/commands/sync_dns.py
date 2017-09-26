faed5caa553d56631330e8eb763113de5b56b66e
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-26 16:09:15 +0200

    Include static records in DNS sync

--- nameserver/management/commands/sync_dns.py
+++ nameserver/management/commands/sync_dns.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from dashboard.settings import parser
 from host.models import Host
-from nameserver.models import Server, Domain
+from nameserver.models import Server, Domain, StaticRecord
 
 class Command(BaseCommand):
   help = ""
@@ -12,5 +12,5 @@ class Command(BaseCommand):
   def handle(self, *args, **options):
     for host in Host.objects.all():
       host.updateDNS()
-    #d = Domain.objects.first()
-    #d.configureA("gw", "192.168.254.2")
+    for sr in StaticRecord.objects.filter(active=True).all():
+      sr.configure()
