1ceab052ca978ca024c206a3f03d9e196a15abf9
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-03 16:15:40 +0200

    Add migrations for new db schema

---
+++ dhcp/migrations/0005_subnet_domain.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('nameserver', '0005_auto_20170926_0912'),
+        ('dhcp', '0004_lease_lease'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='subnet',
+            name='domain',
+            field=models.ForeignKey(null=True, to='nameserver.Domain'),
+        ),
+    ]
