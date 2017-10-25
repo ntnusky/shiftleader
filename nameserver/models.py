2cc9daea500d45b2d6473c31c3a9a1f113aa9624
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-25 15:12:25 +0200

    Only create reverse for static-record if it is to an IP which does not have a host-mapping

--- nameserver/models.py
+++ nameserver/models.py
@@ -159,7 +159,10 @@ class StaticRecord(models.Model):
       try:
         reverseDomain = "%s.%s.%s.in-addr.arpa" % (ip[2], ip[1], ip[0])
         domain = Domain.objects.get(name=reverseDomain)
-        domain.configure(ip[3], "%s.%s." % (self.name, self.domain))
+        try:
+          lease = Lease.objects.get(IP=ip)
+        except Lease.DoesNotExist:
+          domain.configure(ip[3], "%s.%s." % (self.name, self.domain))
       except Domain.DoesNotExist:
         pass
     else:
