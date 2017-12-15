3b24b894794724ccf1ec38e447bdb5414218950e
Eigil Obrestad <eigil-git@obrestad.org>
2017-12-15 15:07:51 +0100

    Added command to clean reports

---
+++ puppet/management/commands/puppet_report_clean.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from puppet.models import Report
+
+class Command(BaseCommand):
+  def add_arguments(self, parser):
+    pass
+    #parser.add_argument('fqdn', type=str)
+
+  def handle(self, *args, **options):
+    invalid = datetime.fromtimestamp(timezone.now().timestamp()-1209600)
+    invalid = timezone.make_aware(invalid)
+    todelete = Report.objects.filter(time__lt=invalid)
+    number = todelete.count()
+    todelete.all().delete()
+
+    self.stdout.write("Deleted %d reports" % number)
