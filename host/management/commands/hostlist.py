b939c56d7bbfbcfea3d9ad674aed1636dfcb9122
Eigil Obrestad <eigil-git@obrestad.org>
2018-01-26 14:59:15 +0100

    Copypasta is delicious

--- host/management/commands/hostlist.py
+++ host/management/commands/hostlist.py
@@ -1,14 +1,9 @@
-import os
-import time
-
 from django.core.management.base import BaseCommand
-from django.template.loader import render_to_string
 
-from dashboard.settings import parser
 from host.models import Host
 
 class Command(BaseCommand):
-  help = "Creates TFTP boot files"
+  help = "Lists hostnames based on the provided filter"
 
   def add_arguments(self, parser):
     parser.add_argument('filter', type=str, nargs='?', default='all')
