38a84cda14efb9b1d842dac36c6d544238b568ea
Eigil Obrestad <eigil-git@obrestad.org>
2018-06-01 10:48:47 +0200

    Only run if needed

--- clients/puppetEnvReport.sh
+++ clients/puppetEnvReport.sh
@@ -4,7 +4,7 @@
 /usr/bin/r10k deploy environment production
 
 for env in $(/opt/shiftleader/manage.py puppet_env2list); do
-  if [[ $(/opt/shiftleader/manage.py puppet_env2update $env) ]]; then
+  if [[ $(/opt/shiftleader/manage.py puppet_env2update $env) -eq '1' ]]; then
     echo "Running r10k to update $env"
     /usr/bin/r10k deploy environment $env -pv
 
