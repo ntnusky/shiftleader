bf175487562964e3c509576b3a1aefd11a703121
Eigil Obrestad <eigil-git@obrestad.org>
2018-06-01 10:47:24 +0200

    New r10k-script

---
+++ clients/puppetEnvReport.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run r10k to discover new environments
+/usr/bin/r10k deploy environment production
+
+for env in $(/opt/shiftleader/manage.py puppet_env2list); do
+  if [[ $(/opt/shiftleader/manage.py puppet_env2update $env) ]]; then
+    echo "Running r10k to update $env"
+    /usr/bin/r10k deploy environment $env -pv
+
+    echo "Reporting the result of the update"
+    /opt/shiftleader/manage.py puppet_env2report $env
+  fi
+done
