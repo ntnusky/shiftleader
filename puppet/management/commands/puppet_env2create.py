21401b549ba713d531739fbbc183a08d41dc6e0b
Eigil Obrestad <eigil-git@obrestad.org>
2018-06-06 14:49:30 +0200

    Add script to add new environments from the r10k repo

---
+++ puppet/management/commands/puppet_env2create.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import yaml
+
+from django.core.management.base import BaseCommand
+from django.utils.timezone import now
+
+from puppet.models import Environment
+
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+    try:
+      FNULL = open(os.devnull, "w")
+      # Grab a list over current environments
+      result = subprocess.run("/usr/bin/r10k deploy display",
+          stdout=subprocess.PIPE, stderr=FNULL, shell=True)
+      r10kenv = yaml.load(result.stdout)[':sources'][0][':environments']
+    except Exception as e: 
+      self.stderr.write("An error occurred: %s" % str(e))
+      return
+
+    # Parse all current environments
+    path = '/etc/puppetlabs/code/environments/'
+    levelInPath = len(path.rstrip('/').split('/'))
+    environments = []
+    for environmentName in r10kenv:
+      try:
+        environment = Environment.objects.get(name=environmentName)
+      except Environment.DoesNotExist:
+        environment = Environment(name=environmentName)
+        self.stdout.write("Creating environment %s" % environmentName)
+
+      environment.last_deployed = now()
+      environment.save()
