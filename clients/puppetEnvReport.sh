03d73b776437942437c4a51ad740b24d04403216
Eigil Obrestad <eigil-git@obrestad.org>
2018-06-06 15:06:01 +0200

    Register new environments as they appear

--- clients/puppetEnvReport.sh
+++ clients/puppetEnvReport.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# If we are the only instance of this script running; start doing something:
 lockdir="/tmp/r10k.run.lock"
 if mkdir $lockdir; then
   # Run r10k to discover new environments
   /usr/bin/r10k deploy environment production
+
+  # Register new environments in shiftleader
+  /opt/shiftleader/manage.py puppet_env2create
   
+  # For each environment shiftleader knows about:
   for env in $(/opt/shiftleader/manage.py puppet_env2list); do
+    # If it is tagged for an update:
     if [[ $(/opt/shiftleader/manage.py puppet_env2update $env) -eq '1' ]]; then
       echo "Running r10k to update $env"
       /usr/bin/r10k deploy environment $env -pv
@@ -14,8 +20,12 @@ if mkdir $lockdir; then
       /opt/shiftleader/manage.py puppet_env2report $env
     fi
   done
+
+  # Remove the lock 
   rmdir $lockdir
   exit 0
+
+# If other instances of the script runs, simply exit.
 else
   echo "Lock failed - exit" >&2
   exit 1
