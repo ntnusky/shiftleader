f49f9a354411bb9422298def66671385841c654a
Eigil Obrestad <eigil-git@obrestad.org>
2020-02-04 12:25:39 +0100

    Make puppetEnvReport log correctly

--- clients/puppetEnvReport.sh
+++ clients/puppetEnvReport.sh
@@ -4,23 +4,23 @@
 lockdir="/tmp/r10k.run.lock"
 if mkdir $lockdir; then
   # Run r10k to discover new environments
-  /usr/bin/r10k deploy environment production
+  /usr/bin/r10k deploy environment production &> /dev/null
 
   # Register new environments in shiftleader
-  /opt/shiftleader/manage.py puppet_env2create
+  /opt/shiftleader/manage.py puppet_env2create &> /dev/null
   
   # For each environment shiftleader knows about:
   for env in $(/opt/shiftleader/manage.py puppet_env2list); do
     # If it is tagged for an update:
-    if [[ $(/opt/shiftleader/manage.py puppet_env2update $env) -eq '1' ]]; then
-      echo "Running r10k to update $env"
-      /usr/bin/r10k deploy environment $env -pv
+    if [[ $(/opt/shiftleader/manage.py puppet_env2update "$env") -eq '1' ]]; then
+      logger "[Puppet-envupdate] Running r10k to update $env"
+      /usr/bin/r10k deploy environment "$env" -pv
 
-      echo "Regenerating puppet types"
-      /opt/puppetlabs/bin/puppet generate types --environment $env &> /dev/null
+      logger "[Puppet-envupdate] Regenerating puppet types"
+      /opt/puppetlabs/bin/puppet generate types --environment "$env" &> /dev/null
   
-      echo "Reporting the result of the update"
-      /opt/shiftleader/manage.py puppet_env2report $env
+      logger "[Puppet-envupdate] Reporting the result of the update"
+      /opt/shiftleader/manage.py puppet_env2report "$env"
     fi
   done
 
@@ -30,6 +30,6 @@ if mkdir $lockdir; then
 
 # If other instances of the script runs, simply exit.
 else
-  echo "Lock failed - exit" >&2
+  echo "$0 cannot run, as it is already running" >&2
   exit 1
 fi
