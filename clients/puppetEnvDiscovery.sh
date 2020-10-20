00e81865215c4b995691885af8f4197f3b0d4786
Eigil Obrestad <eigil-git@obrestad.org>
2020-10-20 16:40:12 +0200

    Add some logging

--- clients/puppetEnvDiscovery.sh
+++ clients/puppetEnvDiscovery.sh
@@ -3,11 +3,15 @@
 while /bin/true; do
   # If a environment-discovery is scheduled:
   if /opt/shiftleader/manage.py puppet_envrefresh; then
+    logger "[Shiftleader-Client] Discovering environments"
+
     # Run r10k to discover new environments
     /usr/bin/r10k deploy environment production &> /dev/null
 
     # Register new environments in shiftleader
     /opt/shiftleader/manage.py puppet_env2create &> /dev/null
+
+    logger "[Shiftleader-Client] Finished reporting new environments"
   fi
   sleep 5
 done
